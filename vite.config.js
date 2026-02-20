46178fb7d78b3b647b7703461ab927789ba306e5
justinecua <jcua.dev@gmail.com>
2026-02-20 13:28:49 +0800

    fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   server: {
     host: "0.0.0.0",
     port: 5173,
